c129a80cc8ba40a757ec01758d7315f8555b475f
Dziamid Zayankouski <dziamid@gmail.com>
2024-12-26 13:46:37 +0400

    dummy change 2

--- express/routes/index.js
+++ express/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pull Request 4' });
+  res.render('index', { title: 'Pull Request 4 Change' });
 });
 
 module.exports = router;
